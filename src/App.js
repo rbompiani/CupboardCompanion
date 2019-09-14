cb8d00123909ddea2ba1bde662a789c074c2a776
Colin Lyons <Colinlyons53@gmail.com>
2019-09-14 10:28:58 -0700

    added Signup component and updated typography in the dashboard component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Cupboard from './components/Cupboard';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 
 
