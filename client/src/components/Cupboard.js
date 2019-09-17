7d92473c44cf5165604a5f439d9d6bb8d7a685b7
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-09-16 20:45:47 -0700

    updated cupboard component to stay within 0-100 bounds

--- client/src/components/Cupboard.js
+++ client/src/components/Cupboard.js
@@ -4,6 +4,9 @@ import Button from '@material-ui/core/Button';
 import NewItemButton from './NewItem';
 
 function Cupboard({data, product}) {
+    if(data<0) {data = 0}
+    else if(data>100){data =100}
+    
     return (
         <>
         <div className="Cupboard">
