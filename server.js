e26aafa5412b609138496511cec5f47c8524dc31
Your Name <you@example.com>
2019-09-18 15:08:50 -0700

    commented out problem code

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const port = process.env.PORT || 5000;
 var mongoose = require("mongoose");
 const session = require('express-session');
 const dbConnection = require('./client/src/config/userDataConnection');
-const MongoStore = require('connect-mongo')(session);
+// const MongoStore = require('connect-mongo')(session);
 const passport = require('./client/src/passport');
 
 // Require all models
 var Sensor = require("./client/src/models/sensorModel");
 
-// Sessions
-app.use(
-	session({
-		secret: 'projectsix', //pick a random string to make the hash that is generated secure
-		store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false, //required
-		saveUninitialized: false //required
-	})
-)
+// // Sessions
+// app.use(
+// 	session({
+// 		secret: 'projectsix', //pick a random string to make the hash that is generated secure
+// 		store: new MongoStore({ mongooseConnection: dbConnection }),
+// 		resave: false, //required
+// 		saveUninitialized: false //required
+// 	})
+// )
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session()); // calls serializeUser and deserializeUser
 
-// Routes
-app.use('/user', user);
+// // Routes
+// app.use('/user', user);
 
 //connect to mongo
 mongoose.connect("mongodb+srv://Rebecca:cupboard@cluster0-vfyb8.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
