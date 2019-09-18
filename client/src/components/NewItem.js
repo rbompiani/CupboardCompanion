6d30d3a79dd2d6ec4852a4d2940f6cf5c43a1b33
Arisham <arielleshamash@gmail.com>
2019-09-17 18:04:08 -0700

    modal is almost functional

--- client/src/components/NewItem.js
+++ client/src/components/NewItem.js
@@ -1,42 +1,69 @@
 import React, {Component} from 'react';
 import Modal from "./Modal";
-
+import Backdrop from "./Backdrop";
 
   class NewItemButton extends Component {
     state = {
       creating: false
     };
 
+    constructor(props) {
+      super(props);
+      this.itemRef = React.createRef();
+      this.weightRef = React.createRef();
+      this.reminderRef = React.createRef();
+    }
+
     startCreateNewItem = () => {
-      this.setState({creating: true});
+      this.setState({ creating: true });
     };
 
     modalConfirmHandler = () => {
-      this.setState({creating: false});
+      this.setState({ creating: false });
+      const item = this.itemRef.current.value;
+      const weight = +this.weightRef.current.value;
+      const reminder = this.reminderRef.current.value;
+
+      const newSensor = {item, weight, reminder};
+      console.log(newSensor);
     };
 
     modalCancelHandler = () => {
-      this.setState({creating: false});
+      this.setState({ creating: false });
     };
 
 
     render() {
       return (
         <React.Fragment>
+          {this.state.creating && <Backdrop />}
           {this.state.creating && (
           <Modal 
-          title="Add Sensor" 
-          canCancel 
-          canConfirm 
-          onCancel={this.modalCancelHandler} 
-          onConfirm={this.modalConfirmHandler}
+            title="Add Sensor" 
+            canCancel 
+            canConfirm 
+            onCancel={this.modalCancelHandler} 
+            onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="item">Item</label>
+                <input type="text" id="item" ref={this.itemRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="weight">Start Weight</label>
+                <input type="number" id="weight" ref={this.weightRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="reminder">Reminder</label>
+                <input type="checkbox" id="reminder" ref={this.reminderRef}></input>
+              </div>
+            </form>
           </Modal>
           )}
-          <div>
+          <div className="events-control">
             <p>Click here to add a new sensor!</p>
-            <button className="btn" onCLick={this.startCreateNewItem}>Add New Sensor</button>
+            <button className="btn" onClick={this.startCreateNewItem}>Add New Sensor</button>
           </div>
         </React.Fragment>
       );
