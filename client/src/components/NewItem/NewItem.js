0eacc7e646e03c11d82fd8afdfa9dbe8397c7326
Vic Caruso <vic.caruso@gmail.com>
2019-09-17 20:36:34 -0700

    deleted duplicate files and fixed import statements

--- client/src/components/NewItem/NewItem.js
+++ client/src/components/NewItem/NewItem.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Modal from "./Modal";
+import Modal from "./Modal.js";
 import Backdrop from "./Backdrop";
 
   class NewItemButton extends Component {
