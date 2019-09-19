80b448f3bd4e1c2c96561bad382e1d5b527416ca
Arisham <arielleshamash@gmail.com>
2019-09-19 14:47:01 -0700

    changing gauge colors

--- client/src/components/Cupboard/Cupboard.js
+++ client/src/components/Cupboard/Cupboard.js
@@ -3,16 +3,24 @@ import Gauge from 'react-svg-gauge';
 import Button from '@material-ui/core/Button';
 import "./Cupboard.css"
 
+
+
 function Cupboard({data, product, link}) {
     if(data<0) {data = 0}
     else if(data>100){data =100}
     
+    var color = {
+        '#fe0400': (data <= 25),
+        'green': (data >= 75),
+        'yellow': (data >=26 && data <= 74)
+    }
+
     return (
         <>
         <div className="Cupboard">
            
             <div className="gauge">
-                <Gauge value={data} width={300} height={240} label={product} />
+                <Gauge value={data} width={300} height={240} label={product} color={'green'}/>
             </div>
             
             <Button color="inherit" href={link} target="new">Restock</Button>
